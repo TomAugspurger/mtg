9721f8f3148b7d3a44ca2d573be4591b123d35e1
Tom Augspurger <tom.augspurger88@gmail.com>
2016-08-25 08:46:24 -0500

    updates

--- plot_transforms.py
+++ plot_transforms.py
@@ -4,27 +4,29 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib as mpl
 
-HEIGHT = .1
-choices = [mpl.colors.rgb2hex(c) for c in sns.color_palette()[:3]]
+FIGSIZE = (4, 6)
+choices = [mpl.colors.rgb2hex(c) for c in sns.color_palette()[:4]]
+N_BLOCKS = 12
+STEP = -1 / N_BLOCKS
 
 
 def plot_factorize():
-    starts = np.arange(0, 1, .1)
+    starts = np.arange(1, 0, STEP)
     np.random.seed(4)
-    colors = np.random.choice(choices, 10)
+    colors = np.random.choice(choices, N_BLOCKS)
     factors, _ = pd.factorize(colors)
 
-    fig, ax = plt.subplots(figsize=(4, 8))
+    fig, ax = plt.subplots(figsize=FIGSIZE)
     for start, c in zip(starts, colors):
         ax.add_patch(
-            mpl.patches.Rectangle((0, start), .4, HEIGHT, facecolor=c),
+            mpl.patches.Rectangle((0, start), .4, STEP, facecolor=c),
         )
 
     for start, c, f in zip(starts, colors, factors):
         ax.add_patch(
-            mpl.patches.Rectangle((.6, start), .4, HEIGHT, fill=False),
+            mpl.patches.Rectangle((.6, start), .399, STEP - .0001, fill=False),
         )
-        ax.annotate(f, xy=(.4, start + .05), xytext=(.8, start + .05),
+        ax.annotate(f, xy=(.4, start - .05), xytext=(.775, start - .05),
                     verticalalignment='center',
                     color=c,
                     fontsize=32)
@@ -37,54 +39,54 @@ def plot_factorize():
     ax.set_axis_bgcolor('none')
     plt.grid(0)
     plt.tight_layout()
-    plt.savefig('factorize.png', dpi=300)
+    plt.savefig('factorize.png', dpi=150, transparent=True)
     plt.close()
 
 
 def plot_get_dummies():
-    WIDTH = .2
-    starts = np.arange(0, 1, .1)
+    WIDTH = .15
+    starts = np.arange(1, 0, -1 / 12)
     np.random.seed(4)
-    colors = np.random.choice(choices, 10)
+    colors = np.random.choice(choices, 12)
     factors, _ = pd.factorize(colors)
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=FIGSIZE)
     for start, c in zip(starts, colors):
         ax.add_patch(
-            mpl.patches.Rectangle((0, start), .2, HEIGHT, facecolor=c),
+            mpl.patches.Rectangle((0, start), WIDTH, STEP, facecolor=c),
         )
 
-    lefts = [.4, .6, .8]
+    lefts = [.4, .55, .7, .85]
     for i, left in enumerate(lefts):
         for start, c, f in zip(starts, colors, factors):
             if i != f:
                 ax.add_patch(
                     mpl.patches.Rectangle((left, start), WIDTH,
-                                          HEIGHT, facecolor='#CECECE')
+                                          STEP, facecolor='#CECECE')
                 )
-                ax.annotate('0', xy=(left + .09, start + .04),
+                ax.annotate('0', xy=(left + .045, start - .05),
                             verticalalignment='center',
                             color='#595959', fontsize=32)
 
             else:
                 ax.add_patch(
-                    mpl.patches.Rectangle((left, start), WIDTH, HEIGHT,
+                    mpl.patches.Rectangle((left, start), WIDTH, STEP,
                                           facecolor=c),
                 )
-                ax.annotate('1', xy=(left + .09, start + .04),
+                ax.annotate('1', xy=(left + .045, start - .05),
                             verticalalignment='center',
                             color='white', fontsize=32)
 
     ax.yaxis.set_visible(False)
     ax.xaxis.tick_top()
-    ax.set_xticks([.1, .5, .7, .9])
-    ax.set_xticklabels(['Raw', 'D1', 'D2', 'D3'])
+    ax.set_xticks([.075, .48, .63, .78, .93])
+    ax.set_xticklabels(['Raw', 'D1', 'D2', 'D3', 'D4'])
     ax.tick_params(axis='x', labelsize=24)
 
     ax.set_axis_bgcolor('none')
     plt.grid(0)
     plt.tight_layout()
-    plt.savefig('dummy.png', dpi=300)
+    plt.savefig('dummy.png', dpi=150, transparent=True)
     plt.close()
 
 
